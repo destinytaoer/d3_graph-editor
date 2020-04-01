5f088e74e87de028bbb872eabc80200e00e043e0
destinytaoer <1848765519@qq.com>
2020-04-01 23:35:00 +0800

    fest[GraphEditor] 完成 Modal 的初始化以及一些辅助函数
    1. 增加三个 Modal 的创建
    2. 增加表单创建方法
    3. 增加获取表单数据方法
    4. 增加可复写的表单项配置方法

--- src/GraphEditor/index.js
+++ src/GraphEditor/index.js
@@ -35,6 +35,8 @@ class GraphEditor {
   constructor(el, data, options = {}) {
     this.el = checkEl(el);
     this.data = data;
+
+    // 配置
     this.toolbarOptions = options.toolbar || {};
     this.searchOptions = options.search || {};
     this.menuOptions = options.menu || {};
@@ -43,23 +45,226 @@ class GraphEditor {
     this.infoOptions = options.info || {};
     this.editOptions = options.edit || {};
     this.type = this.graphOptions.type || 'force';
-  }
-  init() {
-    this.toolbar = new Toolbar(this.el, this.type, this.toolbarOptions);
-    this.toolbar.init();
+
+    // 各模块实例
+    this.eventProxy = new EventEmitter();
+    this.cache = new Cache();
     this.graph =
       this.type === 'force'
         ? new Force(this.el, this.data, this.graphOptions)
         : new Tree(this.el, this.data, this.graphOptions);
-    this.graph.render();
+    this.toolbar = new Toolbar(this.el, this.type, this.toolbarOptions);
     this.info = new Info(this.el, this.infoOptions);
-    this.info.init(this.graph.getCount());
     this.search = new Search(this.el, this.searchOptions);
-    this.search.init();
     this.modal = new Modal(this.el, this.modalOptions);
-    this.modal.init();
     this.menu = new Menu(this.el, this.menuOptions);
+  }
+  init() {
+    this.cache.init(this.data);
+    this.graph.render();
+    this.toolbar.init();
+    this.info.init(this.graph.getCount());
+    this.search.init();
+    this.modal.init();
     this.menu.init();
+    this.createModal();
+  }
+  createModal() {
+    this.createVertexModal();
+    this.createEdgeModal();
+    this.createOutModal();
+  }
+  getVertexFormConfig() {
+    return [
+      {
+        name: 'name',
+        content: '名称',
+        type: 'text'
+      },
+      {
+        name: 'type',
+        content: '类型',
+        type: 'select',
+        options: [
+          {
+            value: 'person',
+            content: '人'
+          },
+          {
+            value: 'company',
+            content: '企业'
+          }
+        ]
+      },
+      {
+        name: 'level',
+        content: '层级',
+        type: 'number',
+        extent: [1, 5]
+      }
+    ];
+  }
+  createVertexModal() {
+    let title = '修改节点信息';
+
+    let body = this.createForm('vertex_form', 'vertex', this.getVertexFormConfig());
+
+    let footer = `
+      <div class="btns">
+        <button type="button" id="vertexCancle" class="btn btn-default">取消</button>
+        <button type="button" id="vertexSave" class="btn btn-info">保存</button>
+      </div>
+    `;
+    this.vertexModal = new Modal(this.el, { title, body, footer });
+  }
+  getEdgeFormConfig() {
+    return [
+      {
+        name: 'label',
+        content: '内容',
+        type: 'text'
+      },
+      {
+        name: 'type',
+        content: '类型',
+        type: 'select',
+        options: [
+          {
+            value: 'invest',
+            content: '投资'
+          },
+          {
+            value: 'self',
+            content: '内部调整'
+          },
+          {
+            value: 'member',
+            content: '成员'
+          }
+        ]
+      }
+    ];
+  }
+  createEdgeModal() {
+    let title = '修改边信息';
+
+    let body = this.createForm('edge_form', 'edge', this.getEdgeFormConfig());
+
+    let footer = `
+      <div class="btns">
+        <button type="button" id="edgeCancle" class="btn btn-default">取消</button>
+        <button type="button" id="edgeSave" class="btn btn-info">保存</button>
+      </div>
+    `;
+    this.edgeModal = new Modal(this.el, { title, body, footer });
+  }
+  createOutModal() {
+    let title = null;
+
+    let body = '<p>您的修改还没有保存, 是否保存并离开?</p>';
+
+    let footer = `
+      <div class="btns">
+        <button type="button" id="outCancle" class="btn btn-default">取消</button>
+        <button type="button" id="outNotSave" class="btn btn-danger">不保存</button>
+        <button type="button" id="outSave" class="btn btn-info">保存</button>
+      </div>
+    `;
+    this.outModal = new Modal(this.el, { title, body, footer });
+  }
+
+  /* 辅助函数 */
+  // 根据配置动态创建表单
+  // TODO: 需要增加 checkbox/radio 等其他表单控件的创建方法
+  createForm(id, type, config) {
+    // id 为 form 的 id
+    // type 为每个 formItem 上 id 的前缀, 防止 id 重复
+    // config 为 formItem 的配置
+    let form = `<form class="form" id="${id}">`;
+
+    config.forEach(c => {
+      let formItem = this[`create${this.toUpperFirst(c.type)}`](c, type);
+      form += formItem;
+    });
+    form += '</form>';
+    return form;
+  }
+  createSelect({ name, content, options }, type) {
+    let select = `
+      <div class="form-item">
+        <label for="${type}_${name}" class="label">${content}</label>
+        <select id="${type}_${name}" class="input" name="${name}">
+      `;
+    options.forEach(option => {
+      select += `<option value="${option.value}">${option.content}</option>`;
+    });
+    select += '</select></div>';
+
+    return select;
+  }
+  createNumber({ name, content, extent }, type) {
+    return `
+      <div class="form-item">
+        <label for="${type}_${name}" class="label">${content}</label>
+        <input type="number" id="${type}_${name}" min="${extent[0]}" max="${extent[1]}" class="input" name="${name}">
+      </div>
+    `;
+  }
+  createText({ name, content }, type) {
+    return `
+      <div class="form-item">
+        <label for="${type}_${name}" class="label">${content}</label>
+        <input type="text" id="${type}_${name}" class="input" name="${name}">
+      </div>`;
+  }
+  // 获取表单数据
+  getFormData(id) {
+    let form = document.getElementById(id);
+    let formArr = Array.prototype.slice.call(form);
+    let data = {};
+
+    formArr.forEach(item => {
+      let { name, value, type } = item;
+      switch (type) {
+        case 'checkbox':
+          if (item.checked) {
+            if (data[name]) {
+              data[name].push(value);
+            } else {
+              data[name] = [value];
+            }
+          }
+          break;
+        case 'radio':
+          if (item.checked) {
+            if (value === 'false') {
+              value = false;
+            }
+            if (value === 'true') {
+              value = true;
+            }
+            data[name] = value;
+          }
+          break;
+        case 'text':
+        default:
+          data[name] = value;
+      }
+    });
+
+    return data;
+  }
+  // 转换为首字母大写
+  toUpperFirst(str) {
+    return str
+      .split('')
+      .map((item, index) => {
+        if (index === 0) {
+          return item.toUpperCase();
+        }
+        return item.toLowerCase();
+      })
+      .join('');
   }
 }
 
